78429a9c8c9eba42d842ed2791b1e410b8519a15
Chris Gibson <cgibson@mrvoxel.com>
2014-10-24 23:42:08 -0700

    added some comments and additional key and mouse callbacks for the menu

--- gamestates/intro.lua
+++ gamestates/intro.lua
@@ -6,23 +6,47 @@
 Gamestate = require "hump.gamestate"
 local Menu = require "gamestates.menu"
 
+-- Gamestate object
 local Intro = {}
 
 
+--
+-- INIT function
+--
+-- This runs at the beginning of the game (or whenever this module is required)
+-- but is guaranteed to run before we enter the gamestate
+--
 function Intro:init()
     self.wait_time = 5   -- show screen for 5 seconds
 end
 
+
+--
+-- ENTER function
+--
+-- Guaranteed to run as soon as we enter the state
+-- This is why we grab the initial time now
+--
 function Intro:enter( previous )
     self.init_time = love.timer.getTime()
 end
 
 
+--
+-- DRAW function
+--
+-- Draw the awesomeness that is the space sheep
+--
 function Intro:draw()
     love.graphics.print("This is the intro sequence!", 400, 300)
 end
 
 
+--
+-- UPDATE function
+--
+-- Check to see if we need to leave the intro screen yet
+--
 function Intro:update(dt)
     if love.timer.getTime() - self.init_time > self.wait_time
     then
--- gamestates/menu.lua
+++ gamestates/menu.lua
@@ -1,11 +1,41 @@
 
 Gamestate = require "hump.gamestate"
 
+-- Menu gamestate object
 local Menu = {}
 
 
+--
+-- DRAW function
+--
+-- Draw the menu in all its glory
+--
 function Menu:draw()
     love.graphics.print("This is the menu!", 400, 300)
+end
+
+
+--
+-- PRESSED functions
+--
+-- Change menu selection based on the key we've presed
+--
+function Menu:keypressed( key, isrepeat )
+end
+
+function Menu:keyreleased( key, isrepeat )
+end
+
+
+--
+-- MOUSE functions
+--
+-- Will also change the menu selection
+--
+function Menu:mousepressed( x, y, mouse )
+end
+
+function Menu:mousereleased( x, y, mouse )
 end
 
 return Menu
